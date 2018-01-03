baf8e2fbb4c5cdef0d7b4cba80053694d599436e
Blake Teeple <teepleblake@gmail.com>
2018-01-03 12:18:56 -0500

    Cleanup & HTML Export
    Cleaned up unused code. Added HTML export and built JS strings. Added ability to automatically open web browser with data. Currently sitting at parent level URLs, moving into children data structures.

--- crawler.py
+++ crawler.py
@@ -1,35 +1,38 @@
 import re
 import urllib.request as req
 import datetime
+import webbrowser
+import os
 
 url_list = []
 starting_url = ""
 sitemap_xml_file_path = ""
-
+parent_urls = []
+parent_filter = ""
+children_temp = []
+longest_url_count = 3
 
 class URL(object):
     def __init__(self, url):
         self.full_url = url
-        self.simple_url = None
+        self.simple_url = url
         self.parent_url = None
         self.has_been_crawled = False
-        if url.count("/") > 3:
+        if url.count("/") > 4:
             indices = [s.start() for s in re.finditer("/", url)]
-            self.parent_url = url[indices[url.count("/") - 2]: indices[url.count("/") - 1]]
-            self.simple_url = url[indices[url.count("/") - 1]:]
-        else:
+            self.parent_url = url[indices[2]: indices[url.count("/") - 1]]
+            if url[indices[2]: indices[url.count("/") - 1]] not in parent_urls:
+                parent_urls.append(url[indices[2]: indices[url.count("/") - 1]])
+        elif url.count("/") == 3:
             self.parent_url = "/"
-            self.simple_url = "/"
-        self.prev_urls = []
-        self.next_urls = []
+        else:
+            indices = [s.start() for s in re.finditer("/", url)]
+            if url[indices[url.count("/") - 2]: indices[url.count("/") - 1]] not in parent_urls:
+                parent_urls.append(url[indices[url.count("/") - 2]: indices[url.count("/") - 1]])
+            self.parent_url = url[indices[url.count("/") - 2]: indices[url.count("/") - 1]]
+
         self.priority = 1.0
         self.change_frequency = "daily"
-
-    def append_prev_url(self, url):
-        self.prev_urls.append(url)
-
-    def append_next_url(self, url):
-        self.next_urls.append(url)
 
 
 class Crawler(object):
@@ -40,22 +43,25 @@ class Crawler(object):
         if sitemap_xml_file_path != "No":
             Sitemap().load(sitemap_xml_file_path)
             self.save()
+            self.build_children()
             VisualSitemap().build(url_list)
         else:
             self.crawl(URL(starting_url))
             for u in url_list:
+                if u.count("/") > longest_url_count:
+                    longest_url_count = u.count("/")
                 if not u.has_been_crawled:
                     self.crawl(u)
-                if len(url_list) >= 10:
+                if len(url_list) >= 50:
                     break
             self.save()
             Sitemap().build(url_list)
+            self.build_children()
             VisualSitemap().build(url_list)
 
     def crawl(self, u):
         x = req.urlopen(u.full_url).read().decode('utf-8')
         for s in re.findall('href="(.*?)"', x, re.S):
-            u.next_urls.append(s)
             u.has_been_crawled = True
             if any(sub in s for sub in ('.css', '.js', '#', '?', 'javascript')):
                 continue
@@ -68,6 +74,9 @@ class Crawler(object):
         with open('urls.txt', 'w') as f:
             for val in url_list:
                 f.writelines(val.full_url + "\n")
+
+    def build_children(self):
+        pass
 
 
 class Sitemap(object):
@@ -96,23 +105,89 @@ class Sitemap(object):
 class VisualSitemap(object):
     def __init__(self):
         self.prefix_html = "<html><head><title>Website Visual Sitemap</title>" \
-						   "<link rel=\"stylesheet\" href=\"js/treant-js-master/Treant.css\">" \
-						   "<script src=\"js/treant-js-master/vendor/raphael.js\"></script>" \
-						   "<script src=\"js/treant-js-master/Treant.min.js\"></script></head>"
+						   "<link rel=\"stylesheet\" href=\"js/Treant.css\">" \
+                           "<link rel=\"stylesheet\" href=\"js/connectors.css\">" \
+						   "<script src=\"js/raphael.js\"></script>" \
+						   "<script src=\"js/Treant.js\"></script></head><body><div id=\"treemap-chart\"></div><script>"
 
-        self.suffix_html = "<body><div id=\"tree-simple\"></div></body></html>"
+        self.suffix_html = "</script></body></html>"
         self.js_string = ""
 
     def build(self, urls):
-        self.js_string = urls
+        js_prefix = "var chart_config = {" \
+                            "chart: {" \
+                            "container: \"#treemap-chart\"," \
+                            "levelSeparation: 25," \
+                            "rootOrientation: \"WEST\"," \
+                            "nodeAlign: \"BOTTOM\"," \
+                            "connectors: {" \
+                            "type: \"step\"," \
+                            "style: {" \
+                            "\"stroke-width\": 1" \
+                            "}" \
+                            "}," \
+                            "node: {" \
+                            "HTMLclass: \"treemap\"" \
+                            "}" \
+                            "}," \
+                            "nodeStructure: {" \
+                            "text: { name: \"Homepage ( / )\" }," \
+                            "connectors: {" \
+                            "style: {" \
+                            "'stroke': '#bbb'," \
+                            "'arrow-end': 'block-wide-long'" \
+                            "}" \
+                            "}," \
+                            "children: ["
+
+        # parent
+        """
+
+                    {
+                        text: { name: "dealers/" },
+                        stackChildren: true,
+                        connectors: {
+                            style: {
+                                'stroke': '#8080FF',
+                                'arrow-end': 'block-wide-long'
+                            }
+                        },
+                        children: [
+                            {
+                                text: {name: "dealers/west_virginia/"}
+                            },
+                            {
+                                text: {name: "dealers/michigan/"}
+                            }
+                        ]
+                    },
+
+        """
+        js_nodes = ""
+        for url in parent_urls:
+            if url.count("/") < 2:
+                js_nodes += "{" \
+                            "text: { name: \"" + url + "\" }," \
+                            "stackChildren: true," \
+                            "connectors: {" \
+                            "style: {" \
+                                "'arrow-end': 'block-wide-long'" \
+                            "}" \
+                            "}" \
+                            "},"
+
+        js_suffix = "]} };new Treant( chart_config );"
+        self.js_string = js_prefix + js_nodes + js_suffix
         self.save()
 
     def save(self):
         with open("index.html", "w") as f:
             f.writelines(self.prefix_html)
-            for value in url_list:
-                f.writelines("<p>{0}</p>".format(value.full_url))
+            f.writelines(self.js_string)
             f.writelines(self.suffix_html)
+            f.writelines(self.suffix_html)
+            webbrowser.open('file://' + os.path.realpath("index.html"))
+
 
 
 if __name__ == '__main__':
