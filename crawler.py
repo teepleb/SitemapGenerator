6e89442a796ca013fb75621362906e6509040b35
Blake Teeple <teepleblake@gmail.com>
2018-01-08 21:24:40 -0500

    Fix Function Calls to Sitemap
    Completed migration to Sitemap file from single file for proper organization. Added commenting for organized code.

--- crawler.py
+++ crawler.py
@@ -14,7 +14,7 @@ class Crawler(object):
 
     def run(self):
         if global_vars.sitemap_xml_file_path != "No":
-            Sitemap().load(global_vars.sitemap_xml_file_path)
+            Sitemap().load()
             self.save()
             self.build_children()
             VisualSitemap().build(global_vars.url_list)
@@ -49,28 +49,6 @@ class Crawler(object):
     def build_children(self):
         pass
 
-
-class Sitemap(object):
-    def __init__(self):
-        pass
-
-    def build(self, urls):
-        with open("sitemap.xml", "w") as f:
-            f.writelines("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">\n")
-            for url in global_vars.url_list:
-                f.writelines("  <url>\n")
-                f.writelines("    <loc>" + url.complete_url + "</loc>\n")
-                f.writelines("    <lastmod>" + datetime.datetime.now().strftime("%Y-%m-%d") + "</lastmod>\n")
-                f.writelines("    <changefreq>" + url.change_frequency + "</changefreq>\n")
-                f.writelines("    <priority>" + str(url.priority) + "</priority>\n")
-                f.writelines("  </url>\n")
-            f.writelines("</urlset>")
-
-    def load(self, file_path):
-        with open(file_path, "r") as f:
-            for line in f:
-                if "<loc>" in line:
-                    global_vars.url_list.append(URL(line.strip()[5:-6]))
 
 class VisualSitemap(object):
     def __init__(self):
@@ -154,7 +132,6 @@ class VisualSitemap(object):
         with open("index.html", "w") as f:
             f.writelines(self.prefix_html)
             f.writelines(self.js_string)
-            f.writelines(self.suffix_html)
             f.writelines(self.suffix_html)
             webbrowser.open('file://' + os.path.realpath("index.html"))
 
--- sitemap.py
+++ sitemap.py
@@ -6,6 +6,7 @@ class Sitemap(object):
     def __init__(self):
         pass
 
+    # builds a sitemap XML with general settings that can be uploaded to the server
     def build(self):
         with open("sitemap.xml", "w") as f:
             f.writelines("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">\n")
@@ -14,13 +15,13 @@ class Sitemap(object):
                 f.writelines("    <loc>" + url.complete_url + "</loc>\n")
                 f.writelines("    <lastmod>" + datetime.datetime.now().strftime("%Y-%m-%d") + "</lastmod>\n")
                 f.writelines("    <changefreq>" + url.change_frequency + "</changefreq>\n")
-                f.writelines("    <priority>" + str(url.priority) + "</priority>\n")
+                f.writelines("    <priority>{0}</priority>\n".format(url.priority))
                 f.writelines("  </url>\n")
             f.writelines("</urlset>")
 
+    # loads URLs from a sitemap to be used to create the visual sitemap
     def load(self):
         with open(global_vars.sitemap_xml_file_path, "r") as f:
             for line in f:
-                print(line)
                 if "<loc>" in line:
                     global_vars.url_list.append(URL(line.strip()[5:-6]))
